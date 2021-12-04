3de9e3cfb7522c96fa516ea8ec2ad45ba0cfc81a
cjparraq <cjparraq@gmail.com>
2021-12-04 17:15:21 -0500

    actualizacion bd

--- database/database.js
+++ database/database.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const connectBD = async () => {
   return await mongoose
-    .connect(process.env.DATABASE_URL)
+    .connect('mongodb://user:user@cluster0-shard-00-00.q6sx0.mongodb.net:27017,cluster0-shard-00-01.q6sx0.mongodb.net:27017,cluster0-shard-00-02.q6sx0.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-11mi5z-shard-0&authSource=admin&retryWrites=true&w=majority')
     .then(() => {
       console.log('Conexion exitosa');
     })
